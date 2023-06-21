0a5a0b37cdd47918f034c386a3943dbb081d41e9
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2023-06-21 15:20:15 +0200

    Update templates to nixos-23.05

--- gtk/flake.nix
+++ gtk/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
